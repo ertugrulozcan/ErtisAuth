a4a6c3937e4f9de84f3635315bcfac142072f6fc
Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2020-12-26 07:56:12 +0300

    Membership bounded services fixed

--- ErtisAuth.Abstractions/Services/Interfaces/IMembershipBoundedCrudService.cs
+++ ErtisAuth.Abstractions/Services/Interfaces/IMembershipBoundedCrudService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Ertis.Core.Collections;
 using ErtisAuth.Core.Models;
@@ -6,6 +7,15 @@ namespace ErtisAuth.Abstractions.Services.Interfaces
 {
 	public interface IMembershipBoundedCrudService<T> where T : IHasMembership
 	{
+		Task<IPaginationCollection<dynamic>> QueryAsync(
+			string query, 
+			int? skip = null, 
+			int? limit = null,
+			bool? withCount = null, 
+			string sortField = null, 
+			SortDirection? sortDirection = null,
+			IDictionary<string, bool> selectFields = null);
+		
 		T Get(string membershipId, string id);
 		
 		Task<T> GetAsync(string membershipId, string id);
@@ -16,5 +26,13 @@ namespace ErtisAuth.Abstractions.Services.Interfaces
 		T Create(string membershipId, T model);
 		
 		Task<T> CreateAsync(string membershipId, T model);
+
+		T Update(string membershipId, T model);
+		
+		Task<T> UpdateAsync(string membershipId, T model);
+
+		bool Delete(string membershipId, string id);
+
+		Task<bool> DeleteAsync(string membershipId, string id);
 	}
 }
--- ErtisAuth.Infrastructure/Services/MembershipBoundedCrudService.cs
+++ ErtisAuth.Infrastructure/Services/MembershipBoundedCrudService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Ertis.Core.Collections;
@@ -9,7 +10,7 @@ using ErtisAuth.Infrastructure.Mapping;
 
 namespace ErtisAuth.Infrastructure.Services
 {
-	public class MembershipBoundedCrudService<TModel, TDto> : IMembershipBoundedCrudService<TModel> 
+	public abstract class MembershipBoundedCrudService<TModel, TDto> : IMembershipBoundedCrudService<TModel> 
 		where TModel : Core.Models.IHasMembership
 		where TDto : IEntity<string>, Dto.Models.IHasMembership
 	{
@@ -31,6 +32,36 @@ namespace ErtisAuth.Infrastructure.Services
 		{
 			this.membershipService = membershipService;
 			this.repository = repository;
+		}
+
+		#endregion
+
+		#region Abstract Methods
+
+		protected abstract bool ValidateModel(TModel model, out IEnumerable<string> errors);
+
+		protected abstract void Overwrite(TModel destination, TModel source);
+
+		protected abstract bool IsAlreadyExist(TModel model, string membershipId);
+		
+		protected abstract Task<bool> IsAlreadyExistAsync(TModel model, string membershipId);
+		
+		protected abstract ErtisAuthException GetAlreadyExistError(TModel model);
+		
+		#endregion
+		
+		#region Query Methods
+
+		public async Task<IPaginationCollection<dynamic>> QueryAsync(
+			string query, 
+			int? skip = null, 
+			int? limit = null, 
+			bool? withCount = null, 
+			string sortField = null,
+			SortDirection? sortDirection = null, 
+			IDictionary<string, bool> selectFields = null)
+		{
+			return await this.repository.QueryAsync(query, skip, limit, withCount, sortField, sortDirection, selectFields);
 		}
 
 		#endregion
@@ -113,14 +144,30 @@ namespace ErtisAuth.Infrastructure.Services
 
 		public virtual TModel Create(string membershipId, TModel model)
 		{
+			// Check membership
 			var membership = this.membershipService.Get(membershipId);
 			if (membership == null)
 			{
 				throw ErtisAuthException.MembershipNotFound(membershipId);
 			}
+			else
+			{
+				model.MembershipId = membershipId;	
+			}
 
-			model.MembershipId = membershipId;
+			// Model validation
+			if (!this.ValidateModel(model, out var errors))
+			{
+				throw ErtisAuthException.ValidationError(errors);
+			}
 			
+			// Check existing
+			if (this.IsAlreadyExist(model, membershipId))
+			{
+				throw this.GetAlreadyExistError(model);
+			}
+			
+			// Insert to database
 			var dto = Mapper.Current.Map<TModel, TDto>(model);
 			var inserted = this.repository.Insert(dto);
 			return Mapper.Current.Map<TDto, TModel>(inserted);
@@ -128,17 +175,129 @@ namespace ErtisAuth.Infrastructure.Services
 		
 		public virtual async Task<TModel> CreateAsync(string membershipId, TModel model)
 		{
+			// Check membership
 			var membership = await this.membershipService.GetAsync(membershipId);
 			if (membership == null)
 			{
 				throw ErtisAuthException.MembershipNotFound(membershipId);
 			}
-
-			model.MembershipId = membershipId;
+			else
+			{
+				model.MembershipId = membershipId;	
+			}
 			
+			// Model validation
+			if (!this.ValidateModel(model, out var errors))
+			{
+				throw ErtisAuthException.ValidationError(errors);
+			}
+			
+			// Check existing
+			if (await this.IsAlreadyExistAsync(model, membershipId))
+			{
+				throw this.GetAlreadyExistError(model);
+			}
+			
+			// Insert to database
 			var dto = Mapper.Current.Map<TModel, TDto>(model);
 			var inserted = await this.repository.InsertAsync(dto);
 			return Mapper.Current.Map<TDto, TModel>(inserted);
+		}
+		
+		#endregion
+
+		#region Update Methods
+
+		public virtual TModel Update(string membershipId, TModel model)
+		{
+			// Check membership
+			var membership = this.membershipService.Get(membershipId);
+			if (membership == null)
+			{
+				throw ErtisAuthException.MembershipNotFound(membershipId);
+			}
+			else
+			{
+				model.MembershipId = membershipId;	
+			}
+
+			// Model validation
+			if (!this.ValidateModel(model, out var errors))
+			{
+				throw ErtisAuthException.ValidationError(errors);
+			}
+			
+			// Check existing
+			if (this.IsAlreadyExist(model, membershipId))
+			{
+				throw this.GetAlreadyExistError(model);
+			}
+			
+			model.MembershipId = membershipId;
+			var dto = Mapper.Current.Map<TModel, TDto>(model);
+			var updated = this.repository.Update(dto);
+			return Mapper.Current.Map<TDto, TModel>(updated);
+		}
+		
+		public virtual async Task<TModel> UpdateAsync(string membershipId, TModel model)
+		{
+			// Check membership
+			var membership = await this.membershipService.GetAsync(membershipId);
+			if (membership == null)
+			{
+				throw ErtisAuthException.MembershipNotFound(membershipId);
+			}
+			else
+			{
+				model.MembershipId = membershipId;	
+			}
+			
+			// Model validation
+			if (!this.ValidateModel(model, out var errors))
+			{
+				throw ErtisAuthException.ValidationError(errors);
+			}
+			
+			// Check existing
+			if (await this.IsAlreadyExistAsync(model, membershipId))
+			{
+				throw this.GetAlreadyExistError(model);
+			}
+			
+			model.MembershipId = membershipId;
+			var dto = Mapper.Current.Map<TModel, TDto>(model);
+			var updated = await this.repository.UpdateAsync(dto);
+			return Mapper.Current.Map<TDto, TModel>(updated);
+		}
+		
+		#endregion
+		
+		#region Delete Methods
+
+		public virtual bool Delete(string membershipId, string id)
+		{
+			var current = this.Get(membershipId, id);
+			if (current != null)
+			{
+				return this.repository.Delete(id);
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+		public virtual async Task<bool> DeleteAsync(string membershipId, string id)
+		{
+			var current = await this.GetAsync(membershipId, id);
+			if (current != null)
+			{
+				return await this.repository.DeleteAsync(id);
+			}
+			else
+			{
+				return false;
+			}
 		}
 		
 		#endregion
