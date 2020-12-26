a79bd20242693c3328c4390af9a4e526425a1924
Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2020-12-26 07:54:24 +0300

    Validation & error handling extensions

--- ErtisAuth.Infrastructure/Exceptions/ErtisAuthException.cs
+++ ErtisAuth.Infrastructure/Exceptions/ErtisAuthException.cs
@@ -15,12 +15,7 @@ namespace ErtisAuth.Infrastructure.Exceptions
 
 		#endregion
 
-		#region Exceptions
-
-		public static ErtisAuthException RequestBodyNull()
-		{
-			return new ErtisAuthException(HttpStatusCode.BadRequest, "Request body is null!", "RequestBodyNull");
-		}
+		#region Validation Exceptions
 		
 		public static ValidationException ValidationError(IEnumerable<string> errors)
 		{
@@ -30,11 +25,101 @@ namespace ErtisAuth.Infrastructure.Exceptions
 			};
 		}
 		
+		#endregion
+		
+		#region Token Exceptions
+		
+		public static ErtisAuthException InvalidToken()
+		{
+			return new ErtisAuthException(HttpStatusCode.Unauthorized, "Provided token is invalid", "InvalidToken");
+		}
+		
+		public static ErtisAuthException TokenTypeNotSupported()
+		{
+			return new ErtisAuthException(HttpStatusCode.BadRequest, "Token type not supported. Token type must be one of Bearer or Basic", "TokenTypeNotSupported");
+		}
+		
+		public static ErtisAuthException TokenWasRevoked()
+		{
+			return new ErtisAuthException(HttpStatusCode.Unauthorized, "Provided token was revoked", "TokenWasRevoked");
+		}
+		
+		public static ErtisAuthException RefreshTokenWasRevoked()
+		{
+			return new ErtisAuthException(HttpStatusCode.Unauthorized, "Provided refresh token was revoked", "RefreshTokenWasRevoked");
+		}
+		
+		public static ErtisAuthException TokenWasExpired()
+		{
+			return new ErtisAuthException(HttpStatusCode.Unauthorized, "Provided token was expired", "TokenWasExpired");
+		}
+		
+		public static ErtisAuthException RefreshTokenWasExpired()
+		{
+			return new ErtisAuthException(HttpStatusCode.Unauthorized, "Provided refresh token was expired", "RefreshTokenWasExpired");
+		}
+		
+		public static ErtisAuthException TokenIsNotRefreshable()
+		{
+			return new ErtisAuthException(HttpStatusCode.Unauthorized, "Provided token is not refreshable", "TokenIsNotRefreshable");
+		}
+		
+		#endregion
+		
+		#region Membership Exceptions
+		
 		public static ErtisAuthException MembershipNotFound(string membershipId)
 		{
 			return new ErtisAuthException(HttpStatusCode.NotFound, $"Membership not found in db by given membership_id: <{membershipId}>", "MembershipNotFound");
 		}
-
+		
+		public static ValidationException MalformedMembership(string membershipId, IEnumerable<string> errors)
+		{
+			return new ValidationException(HttpStatusCode.NotImplemented, $"Membership is not configured properly (membership_id: '{membershipId}')", "MalformedMembership")
+			{
+				Errors = errors
+			};
+		}
+		
+		#endregion
+		
+		#region User Exceptions
+		
+		public static ErtisAuthException UserNotFound(string field, string parameterName)
+		{
+			return new ErtisAuthException(HttpStatusCode.NotFound, $"User not found in db by given {parameterName}: <{field}>", "UserNotFound");
+		}
+		
+		public static ErtisAuthException UsernameOrPasswordIsWrong(string username, string password)
+		{
+			return new ErtisAuthException(HttpStatusCode.Unauthorized, "Username or password is wrong", "UsernameOrPasswordIsWrong");
+		}
+		
+		public static ErtisAuthException UserWithSameUsernameAlreadyExists(string usernameOrEmail)
+		{
+			return new ErtisAuthException(HttpStatusCode.Conflict, $"The user with same username or email is already exists ({usernameOrEmail})", "UserWithSameUsernameAlreadyExists");
+		}
+		
+		#endregion
+		
+		#region Application Exceptions
+		
+		
+		
+		#endregion
+		
+		#region Role Exceptions
+		
+		public static ErtisAuthException RoleNotFound(string roleId)
+		{
+			return new ErtisAuthException(HttpStatusCode.NotFound, $"Role not found in db by given _id: <{roleId}>", "RoleNotFound");
+		}
+		
+		public static ErtisAuthException RoleWithSameNameAlreadyExists(string name)
+		{
+			return new ErtisAuthException(HttpStatusCode.Conflict, $"The role with same name is already exists ({name})", "RoleWithSameNameAlreadyExists");
+		}
+		
 		#endregion
 	}
 }
