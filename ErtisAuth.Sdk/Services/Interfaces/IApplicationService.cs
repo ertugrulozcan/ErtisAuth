ad2714b0ab2750ff92fdbb141cbcd4f738817f79
Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2021-04-05 19:35:47 +0300

    ApplicationService implemented

--- ErtisAuth.Sdk/Services/Interfaces/IApplicationService.cs
+++ ErtisAuth.Sdk/Services/Interfaces/IApplicationService.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Ertis.Core.Collections;
 using Ertis.Core.Models.Response;
 using ErtisAuth.Core.Models.Applications;
 using ErtisAuth.Core.Models.Identity;
@@ -7,8 +8,28 @@ namespace ErtisAuth.Sdk.Services.Interfaces
 {
 	public interface IApplicationService
 	{
-		IResponseResult<Application> GetApplication(string id, TokenBase token);
+		IResponseResult<Application> CreateApplication(Application application, TokenBase token);
 		
-		Task<IResponseResult<Application>> GetApplicationAsync(string id, TokenBase token);
+		Task<IResponseResult<Application>> CreateApplicationAsync(Application application, TokenBase token);
+		
+		IResponseResult<Application> GetApplication(string applicationId, TokenBase token);
+		
+		Task<IResponseResult<Application>> GetApplicationAsync(string applicationId, TokenBase token);
+		
+		IResponseResult<IPaginationCollection<Application>> GetApplications(TokenBase token, int? skip = null, int? limit = null, bool? withCount = null, string orderBy = null, SortDirection? sortDirection = null, string searchKeyword = null);
+		
+		Task<IResponseResult<IPaginationCollection<Application>>> GetApplicationsAsync(TokenBase token, int? skip = null, int? limit = null, bool? withCount = null, string orderBy = null, SortDirection? sortDirection = null, string searchKeyword = null);
+		
+		IResponseResult<IPaginationCollection<Application>> QueryApplications(TokenBase token, string query, int? skip = null, int? limit = null, bool? withCount = null, string orderBy = null, SortDirection? sortDirection = null);
+		
+		Task<IResponseResult<IPaginationCollection<Application>>> QueryApplicationsAsync(TokenBase token, string query, int? skip = null, int? limit = null, bool? withCount = null, string orderBy = null, SortDirection? sortDirection = null);
+		
+		IResponseResult<Application> UpdateApplication(Application application, TokenBase token);
+		
+		Task<IResponseResult<Application>> UpdateApplicationAsync(Application application, TokenBase token);
+		
+		IResponseResult DeleteApplication(string applicationId, TokenBase token);
+		
+		Task<IResponseResult> DeleteApplicationAsync(string applicationId, TokenBase token);
 	}
 }
