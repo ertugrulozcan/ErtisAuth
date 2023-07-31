af3028d229f0a76b0742ada622a7cb69dfc05554
Ahmet Ertuğrul Özcan <ertugrul.ozcan@odeontechnology.com>
2023-07-31 12:47:27 +0300

    Diagnostics endpoint

--- ErtisAuth.WebAPI/Controllers/DiagnosticsController.cs
+++ ErtisAuth.WebAPI/Controllers/DiagnosticsController.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using ErtisAuth.WebAPI.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ErtisAuth.WebAPI.Controllers;
@@ -11,7 +14,8 @@ public class DiagnosticsController : ControllerBase
 	#region Methods
 
 	[HttpGet("memory")]
-	public IActionResult GetMemory()
+	[SuppressMessage("ReSharper", "IdentifierTypo")]
+	public IActionResult GetMemoryUsage()
 	{
 		using (var process = Process.GetCurrentProcess())
 		{
@@ -20,8 +24,10 @@ public class DiagnosticsController : ControllerBase
 			var @virtual = process.VirtualMemorySize64;
 			var system = process.NonpagedSystemMemorySize64;
 			var total = GC.GetTotalMemory(false);
+			var used = SystemDiagnostics.GetUsedMemoryForAllProcesses();
+			var size = SystemDiagnostics.GetTotalMemoryInKb() * 1024;
 			var gc = GC.GetGCMemoryInfo();
-			
+
 			return this.Ok(new
 			{
 				paged, 
@@ -29,9 +35,20 @@ public class DiagnosticsController : ControllerBase
 				@virtual, 
 				system, 
 				total, 
+				used,
+				size, 
 				gc
 			});
 		}
+	}
+	
+	[HttpGet("cpu")]
+	public IActionResult GetCpuUsage()
+	{
+		return this.Ok(new
+		{
+			overall = SystemDiagnostics.GetOverallCpuUsage()
+		});
 	}
 
 	#endregion
---
+++ ErtisAuth.WebAPI/Helpers/SystemDiagnostics.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ErtisAuth.WebAPI.Helpers;
+
+internal static class SystemDiagnostics
+{
+	private static long totalMemoryInKb;
+	
+	/// <summary>
+	/// Get the system overall CPU usage percentage.
+    /// </summary>
+    /// <returns>The percentage value with the '%' sign. e.g. if the usage is 30.1234 %, then it will return 30.12.</returns>
+    public static double GetOverallCpuUsage()
+    {
+	    try
+	    {
+		    var startTime = DateTime.UtcNow;
+		    var startCpuUsage = Process.GetProcesses().Sum(a => a.TotalProcessorTime.TotalMilliseconds);
+	    
+		    System.Threading.Thread.Sleep(500);
+	    
+		    var endTime = DateTime.UtcNow;
+		    var endCpuUsage = Process.GetProcesses().Sum(a => a.TotalProcessorTime.TotalMilliseconds);
+	    
+		    var cpuUsedMs = endCpuUsage - startCpuUsage;
+		    var totalMsPassed = (endTime - startTime).TotalMilliseconds;
+		    var cpuUsageTotal = cpuUsedMs / (Environment.ProcessorCount * totalMsPassed);
+	    
+		    return Math.Round(cpuUsageTotal * 100, 2);
+	    }
+	    catch (Exception e)
+	    {
+		    Console.WriteLine(e);
+		    return 0;
+	    }
+    }
+
+    /// <summary>
+    /// Get the system overall memory usage percentage.
+    /// </summary>
+    /// <returns>The percentage value with the '%' sign. e.g. if the usage is 30.1234 %, then it will return 30.12.</returns>
+    public static double GetOccupiedMemoryPercentage()
+    {
+	    try
+	    {
+		    var totalMemory = GetTotalMemoryInKb() * 1024;
+		    var usedMemory = GetUsedMemoryForAllProcesses();
+	    
+		    var percentage = (usedMemory * 100.0) / totalMemory;
+		    return Math.Round(percentage, 2);
+	    }
+	    catch (Exception e)
+	    {
+		    Console.WriteLine(e);
+		    return 0;
+	    }
+    }
+    
+    internal static double GetUsedMemoryForAllProcesses()
+    {
+	    try
+	    {
+		    var totalAllocatedMemoryInBytes = Process.GetProcesses().Sum(a => a.PrivateMemorySize64);
+		    return totalAllocatedMemoryInBytes;
+	    }
+	    catch (Exception e)
+	    {
+		    Console.WriteLine(e);
+		    return 0;
+	    }
+    }
+
+    internal static long GetTotalMemoryInKb()
+    {
+	    try
+	    {
+		    // only parse the file once
+		    if (totalMemoryInKb > 0)
+		    {
+			    return totalMemoryInKb;
+		    }
+	    
+		    // ReSharper disable once StringLiteralTypo
+		    const string path = "/proc/meminfo";
+		    if (!File.Exists(path))
+		    {
+			    throw new FileNotFoundException($"File not found: {path}");
+		    }
+	    
+		    using (var reader = new StreamReader(path))
+		    {
+			    string line;
+			    while (!string.IsNullOrWhiteSpace(line = reader.ReadLine()))
+			    {
+				    if (line.Contains("MemTotal", StringComparison.OrdinalIgnoreCase))
+				    {
+					    // e.g. MemTotal: 16370152 kB
+					    var parts = line.Split(':');
+					    var valuePart = parts[1].Trim();
+					    parts = valuePart.Split(' ');
+					    var numberString = parts[0].Trim();
+				    
+					    var result = long.TryParse(numberString, out totalMemoryInKb);
+					    return result ? totalMemoryInKb : throw new Exception($"Cannot parse 'MemTotal' value from the file {path}.");
+				    }
+			    }
+		    
+			    throw new Exception($"Cannot find the 'MemTotal' property from the file {path}.");
+		    }
+	    }
+	    catch (Exception e)
+	    {
+		    Console.WriteLine(e);
+		    return 0;
+	    }
+    }
+}
