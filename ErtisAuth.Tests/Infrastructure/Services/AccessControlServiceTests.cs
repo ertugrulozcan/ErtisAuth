f2f5513fe7598b1f8c41dcb9e43f2d891c1621ab
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2024-03-20 23:37:08 +0300

    Unit tests fixed

--- ErtisAuth.Tests/Infrastructure/Services/AccessControlServiceTests.cs
+++ ErtisAuth.Tests/Infrastructure/Services/AccessControlServiceTests.cs
@@ -75,7 +75,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		public void Permission_Check_All_Users_Read_All_With_Permit_User_Return_True_Test()
 		{
 			var role = this.roleService.GetBySlug("admin", "test_membership");
-			const string rbac = "*.users.read.test_utilizer";
+			const string rbac = "*.users.read.user_1";
 			var hasPermission = this.accessControlService.HasPermission(role, rbac);
 			Assert.That(hasPermission);
 		}
@@ -84,7 +84,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		public void Permission_Check_All_Users_Create_All_With_Permit_User_Return_True_Test()
 		{
 			var role = this.roleService.GetBySlug("admin", "test_membership");
-			const string rbac = "*.users.create.test_utilizer";
+			const string rbac = "*.users.create.user_1";
 			var hasPermission = this.accessControlService.HasPermission(role, rbac);
 			Assert.That(hasPermission);
 		}
@@ -93,7 +93,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		public void Permission_Check_All_Users_Update_All_With_Permit_User_Return_True_Test()
 		{
 			var role = this.roleService.GetBySlug("admin", "test_membership");
-			const string rbac = "*.users.update.test_utilizer";
+			const string rbac = "*.users.update.user_1";
 			var hasPermission = this.accessControlService.HasPermission(role, rbac);
 			Assert.That(hasPermission);
 		}
@@ -102,7 +102,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		public void Permission_Check_All_Users_Delete_All_With_Permit_User_Return_True_Test()
 		{
 			var role = this.roleService.GetBySlug("admin", "test_membership");
-			const string rbac = "*.users.delete.test_utilizer";
+			const string rbac = "*.users.delete.user_1";
 			var hasPermission = this.accessControlService.HasPermission(role, rbac);
 			Assert.That(hasPermission);
 		}
@@ -111,7 +111,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		public void Permission_Check_All_Users_Read_All_With_Forbid_User_Return_True_Test()
 		{
 			var role = this.roleService.GetBySlug("readonly", "test_membership");
-			const string rbac = "*.users.read.forbid_user_id";
+			const string rbac = "*.users.read.user_2";
 			var hasPermission = this.accessControlService.HasPermission(role, rbac);
 			Assert.That(hasPermission);
 		}
@@ -120,7 +120,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		public void Permission_Check_All_Users_Create_All_With_Forbid_User_Return_False_Test()
 		{
 			var role = this.roleService.GetBySlug("readonly", "test_membership");
-			const string rbac = "*.users.create.forbid_user_id";
+			const string rbac = "*.users.create.user_2";
 			var hasPermission = this.accessControlService.HasPermission(role, rbac);
 			Assert.That(!hasPermission);
 		}
@@ -129,7 +129,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		public void Permission_Check_All_Users_Update_All_With_Forbid_User_Return_False_Test()
 		{
 			var role = this.roleService.GetBySlug("readonly", "test_membership");
-			const string rbac = "*.users.update.forbid_user_id";
+			const string rbac = "*.users.update.user_2";
 			var hasPermission = this.accessControlService.HasPermission(role, rbac);
 			Assert.That(!hasPermission);
 		}
@@ -138,7 +138,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		public void Permission_Check_All_Users_Delete_All_With_Forbid_User_Return_False_Test()
 		{
 			var role = this.roleService.GetBySlug("readonly", "test_membership");
-			const string rbac = "*.users.delete.forbid_user_id";
+			const string rbac = "*.users.delete.user_2";
 			var hasPermission = this.accessControlService.HasPermission(role, rbac);
 			Assert.That(!hasPermission);
 		}
@@ -146,7 +146,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		[Test]
 		public void Ubac_Permission_Check_All_Users_Read_All_Return_True_Test()
 		{
-			var dynamicObject = this.userService.GetAsync("test_membership", "restricted_user_id").ConfigureAwait(false).GetAwaiter().GetResult();
+			var dynamicObject = this.userService.GetAsync("test_membership", "user_1").ConfigureAwait(false).GetAwaiter().GetResult();
 			var user = dynamicObject.Deserialize<User>();
 			const string rbac = "*.users.read.*";
 			var hasPermission = this.accessControlService.HasPermission(user, rbac);
@@ -156,17 +156,17 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		[Test]
 		public void Ubac_Permission_Check_All_Users_Create_All_Return_False_Test()
 		{
-			var dynamicObject = this.userService.GetAsync("test_membership", "restricted_user_id").ConfigureAwait(false).GetAwaiter().GetResult();
+			var dynamicObject = this.userService.GetAsync("test_membership", "user_1").ConfigureAwait(false).GetAwaiter().GetResult();
 			var user = dynamicObject.Deserialize<User>();
 			const string rbac = "*.users.create.*";
 			var hasPermission = this.accessControlService.HasPermission(user, rbac);
-			Assert.That(!hasPermission);
+			Assert.That(hasPermission);
 		}
 
 		[Test]
 		public void Ubac_Permission_Check_All_Users_Update_All_Return_True_Test()
 		{
-			var dynamicObject = this.userService.GetAsync("test_membership", "restricted_user_id").ConfigureAwait(false).GetAwaiter().GetResult();
+			var dynamicObject = this.userService.GetAsync("test_membership", "user_1").ConfigureAwait(false).GetAwaiter().GetResult();
 			var user = dynamicObject.Deserialize<User>();
 			const string rbac = "*.users.update.*";
 			var hasPermission = this.accessControlService.HasPermission(user, rbac);
@@ -176,7 +176,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		[Test]
 		public void Ubac_Permission_Check_All_Users_Delete_All_Return_True_Test()
 		{
-			var dynamicObject = this.userService.GetAsync("test_membership", "restricted_user_id").ConfigureAwait(false).GetAwaiter().GetResult();
+			var dynamicObject = this.userService.GetAsync("test_membership", "user_1").ConfigureAwait(false).GetAwaiter().GetResult();
 			var user = dynamicObject.Deserialize<User>();
 			const string rbac = "*.users.delete.*";
 			var hasPermission = this.accessControlService.HasPermission(user, rbac);
@@ -186,7 +186,7 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		[Test]
 		public void Ubac_Permission_Check_All_Users_Create_All_Return_True_Test()
 		{
-			var dynamicObject = this.userService.GetAsync("test_membership", "qualified_user_id").ConfigureAwait(false).GetAwaiter().GetResult();
+			var dynamicObject = this.userService.GetAsync("test_membership", "user_1").ConfigureAwait(false).GetAwaiter().GetResult();
 			var user = dynamicObject.Deserialize<User>();
 			const string rbac = "*.users.create.*";
 			var hasPermission = this.accessControlService.HasPermission(user, rbac);
@@ -196,21 +196,21 @@ namespace ErtisAuth.Tests.Infrastructure.Services
 		[Test]
 		public void Ubac_Permission_Check_All_Users_Update_All_Return_False_Test()
 		{
-			var dynamicObject = this.userService.GetAsync("test_membership", "qualified_user_id").ConfigureAwait(false).GetAwaiter().GetResult();
+			var dynamicObject = this.userService.GetAsync("test_membership", "user_1").ConfigureAwait(false).GetAwaiter().GetResult();
 			var user = dynamicObject.Deserialize<User>();
 			const string rbac = "*.users.update.*";
 			var hasPermission = this.accessControlService.HasPermission(user, rbac);
-			Assert.That(!hasPermission);
+			Assert.That(hasPermission);
 		}
 		
 		[Test]
 		public void Ubac_Permission_Check_All_Users_Delete_All_Return_False_Test()
 		{
-			var dynamicObject = this.userService.GetAsync("test_membership", "qualified_user_id").ConfigureAwait(false).GetAwaiter().GetResult();
+			var dynamicObject = this.userService.GetAsync("test_membership", "user_1").ConfigureAwait(false).GetAwaiter().GetResult();
 			var user = dynamicObject.Deserialize<User>();
 			const string rbac = "*.users.delete.*";
 			var hasPermission = this.accessControlService.HasPermission(user, rbac);
-			Assert.That(!hasPermission);
+			Assert.That(hasPermission);
 		}
 		
 		#endregion
--- ErtisAuth.Tests/Mocks/Services/MockUserService.cs
+++ ErtisAuth.Tests/Mocks/Services/MockUserService.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Ertis.Core.Collections;
+using Ertis.Core.Models.Resources;
 using Ertis.Schema.Dynamics;
+using Ertis.Schema.Extensions;
 using ErtisAuth.Abstractions.Services.Interfaces;
 using ErtisAuth.Core.Models.Identity;
 using ErtisAuth.Core.Models.Users;
@@ -13,16 +16,70 @@ namespace ErtisAuth.Tests.Mocks.Services
 {
     public class MockUserService : IUserService
     {
+        #region Mock Data
+
+        private List<User> MockUsers => new()
+        {
+            new User
+            {
+                Id = "user_1",
+                MembershipId = "test_membership",
+                FirstName = "Rick",
+                LastName = "Sanchez",
+                Username = "rick.sanchez",
+                EmailAddress = "rick.sanchez@adultswim.com",
+                Role = "admin",
+                UserType = "user",
+                SourceProvider = "ErtisAuth",
+                IsActive = true,
+                Sys = new SysModel
+                {
+                    CreatedAt = new DateTime(),
+                    CreatedBy = "admin"
+                }
+            },
+            new User
+            {
+                Id = "user_2",
+                MembershipId = "test_membership",
+                FirstName = "Morty",
+                LastName = "Smith",
+                Username = "morty.smith",
+                EmailAddress = "morty.smith@adultswim.com",
+                Role = "admin",
+                UserType = "user",
+                SourceProvider = "ErtisAuth",
+                IsActive = true,
+                Sys = new SysModel
+                {
+                    CreatedAt = new DateTime(),
+                    CreatedBy = "admin"
+                }
+            }
+        };
+
+        #endregion
+        
+        #region Events
+
         public event EventHandler<CreateResourceEventArgs<DynamicObject>> OnCreated;
         
         public event EventHandler<UpdateResourceEventArgs<DynamicObject>> OnUpdated;
         
         public event EventHandler<DeleteResourceEventArgs<DynamicObject>> OnDeleted;
 
+        #endregion
+        
+        #region Constructors
+
         public MockUserService()
         {
             PreventNeverUsedEventsWarning();
         }
+
+        #endregion
+        
+        #region Methods
         
         private void PreventNeverUsedEventsWarning()
         {
@@ -31,17 +88,20 @@ namespace ErtisAuth.Tests.Mocks.Services
             this.OnDeleted?.Invoke(this, null);
         }
         
-        public Task<DynamicObject> GetAsync(string membershipId, string id, CancellationToken cancellationToken = default)
+        public async Task<DynamicObject> GetAsync(string membershipId, string id, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await Task.CompletedTask;
+            var user = this.MockUsers.FirstOrDefault(x => x.MembershipId == membershipId && x.Id == id);
+            return user != null ? new DynamicObject(user) : null;
         }
 
-        public Task<User> GetFromCacheAsync(string membershipId, string id, CancellationToken cancellationToken = default)
+        public async Task<User> GetFromCacheAsync(string membershipId, string id, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await Task.CompletedTask;
+            return this.MockUsers.FirstOrDefault(x => x.MembershipId == membershipId && x.Id == id);
         }
 
-        public Task<IPaginationCollection<DynamicObject>> GetAsync(
+        public async Task<IPaginationCollection<DynamicObject>> GetAsync(
             string membershipId, 
             int? skip = null, 
             int? limit = null, 
@@ -50,103 +110,258 @@ namespace ErtisAuth.Tests.Mocks.Services
             SortDirection? sortDirection = null, 
             CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await Task.CompletedTask;
+            return new PaginationCollection<DynamicObject>
+            {
+                Count = this.MockUsers.Count,
+                Items = this.MockUsers
+                    .Where(x => x.MembershipId == membershipId)
+                    .Skip(skip ?? 0)
+                    .Take(limit ?? 500)
+                    .Select(x => new DynamicObject(x))
+            };
         }
         
-        public Task<User> GetByUsernameOrEmailAddressAsync(string membershipId, string usernameOrEmailAddress)
+        public async Task<User> GetByUsernameOrEmailAddressAsync(string membershipId, string usernameOrEmailAddress)
         {
-            throw new NotImplementedException();
+            await Task.CompletedTask;
+            return this.MockUsers.FirstOrDefault(x =>
+                x.MembershipId == membershipId && 
+                x.Username == usernameOrEmailAddress ||
+                x.EmailAddress == usernameOrEmailAddress);
         }
 
-        public Task<DynamicObject> CreateAsync(Utilizer utilizer, string membershipId, DynamicObject model, string host = null, CancellationToken cancellationToken = default)
+        public async Task<DynamicObject> CreateAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            DynamicObject model, 
+            string host = null, 
+            CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await Task.CompletedTask;
+            var user = model.Deserialize<User>();
+            user.MembershipId = membershipId;
+            this.MockUsers.Add(user);
+            return model;
         }
 
-        public Task<string> SendActivationMailAsync(string membershipId, string userId, string host = null, CancellationToken cancellationToken = default)
+        public async Task<DynamicObject> UpdateAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string userId, 
+            DynamicObject model, 
+            bool fireEvent = true, 
+            CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<DynamicObject> UpdateAsync(Utilizer utilizer, string membershipId, string userId, DynamicObject model, bool fireEvent = true, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
+            var current = await this.GetAsync(membershipId, userId, cancellationToken: cancellationToken);
+            if (current != null)
+            {
+                current.Merge(model);
+                this.MockUsers[this.MockUsers.FindIndex(x => x.Id == userId)] = current.Deserialize<User>();
+                return current;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public bool Delete(Utilizer utilizer, string membershipId, string id)
         {
-            throw new NotImplementedException();
+            var index = this.MockUsers.FindIndex(x => x.Id == id);
+            if (index >= 0)
+            {
+                this.MockUsers.RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        ValueTask<bool> IDeletableMembershipBoundedService.DeleteAsync(Utilizer utilizer, string membershipId, string id, CancellationToken cancellationToken)
+        async ValueTask<bool> IDeletableMembershipBoundedService.DeleteAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string id, 
+            CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+            return this.Delete(utilizer, membershipId, id);
+        }
+
+        public bool? BulkDelete(
+            Utilizer utilizer, 
+            string membershipId, 
+            string[] ids)
+        {
+            var allDeleted = true;
+            foreach (var id in ids)
+            {
+                allDeleted = allDeleted && this.Delete(utilizer, membershipId, id);
+            }
+
+            return allDeleted;
+        }
+
+        async ValueTask<bool?> IDeletableMembershipBoundedService.BulkDeleteAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string[] ids, 
+            CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+            return this.BulkDelete(utilizer, membershipId, ids);
+        }
+
+        public async Task<bool> DeleteAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string id, 
+            CancellationToken cancellationToken = default)
+        {
+            await Task.CompletedTask;
+            return this.Delete(utilizer, membershipId, id);
+        }
+
+        public async Task<bool?> BulkDeleteAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string[] ids, 
+            CancellationToken cancellationToken = default)
+        {
+            await Task.CompletedTask;
+            return this.BulkDelete(utilizer, membershipId, ids);
+        }
+
+        public async Task<IPaginationCollection<DynamicObject>> QueryAsync(
+            string membershipId, 
+            string query, 
+            int? skip = null, 
+            int? limit = null, 
+            bool? withCount = null,
+            string orderBy = null, 
+            SortDirection? sortDirection = null, 
+            IDictionary<string, bool> selectFields = null, 
+            CancellationToken cancellationToken = default)
+        {
+            return await this.GetAsync(
+                membershipId, 
+                skip, 
+                limit, 
+                withCount ?? false, 
+                orderBy, 
+                sortDirection,
+                cancellationToken: cancellationToken);
+        }
+
+        public async Task<IPaginationCollection<DynamicObject>> SearchAsync(
+            string membershipId, 
+            string keyword, 
+            int? skip = null, 
+            int? limit = null, 
+            bool? withCount = null,
+            string orderBy = null, 
+            SortDirection? sortDirection = null, 
+            CancellationToken cancellationToken = default)
+        {
+            return await this.GetAsync(
+                membershipId, 
+                skip, 
+                limit, 
+                withCount ?? false, 
+                orderBy, 
+                sortDirection,
+                cancellationToken: cancellationToken);
+        }
+
+        public async Task<UserWithPasswordHash> GetUserWithPasswordAsync(
+            string id, 
+            string membershipId, 
+            CancellationToken cancellationToken = default)
+        {
+            return (await this.GetAsync(membershipId, id, cancellationToken: cancellationToken)).Deserialize<UserWithPasswordHash>();
+        }
+
+        public async Task<UserWithPasswordHash> GetUserWithPasswordAsync(
+            string username, 
+            string email, 
+            string membershipId, 
+            CancellationToken cancellationToken = default)
+        {
+            return (await this.GetByUsernameOrEmailAddressAsync(membershipId, username)) as UserWithPasswordHash;
+        }
+        
+        public Task<User> ActivateUserAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string activationCode, 
+            CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public Task<User> ActivateUserByIdAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string userId, 
+            CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public Task<User> FreezeUserByIdAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string userId, 
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public bool? BulkDelete(Utilizer utilizer, string membershipId, string[] ids)
+        public Task<DynamicObject> ChangePasswordAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string userId, 
+            string newPassword, 
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        ValueTask<bool?> IDeletableMembershipBoundedService.BulkDeleteAsync(Utilizer utilizer, string membershipId, string[] ids, CancellationToken cancellationToken)
+        public Task<ResetPasswordToken> ResetPasswordAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string emailAddress, 
+            string host, 
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> DeleteAsync(Utilizer utilizer, string membershipId, string id, CancellationToken cancellationToken = default)
+        public Task<User> VerifyResetTokenAsync(
+            string membershipId, 
+            string resetToken, 
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool?> BulkDeleteAsync(Utilizer utilizer, string membershipId, string[] ids, CancellationToken cancellationToken = default)
+        public Task SetPasswordAsync(
+            Utilizer utilizer, 
+            string membershipId, 
+            string resetToken, 
+            string usernameOrEmailAddress,
+            string password, 
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IPaginationCollection<DynamicObject>> QueryAsync(string membershipId, string query, int? skip = null, int? limit = null, bool? withCount = null,
-            string orderBy = null, SortDirection? sortDirection = null, IDictionary<string, bool> selectFields = null, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IPaginationCollection<DynamicObject>> SearchAsync(string membershipId, string keyword, int? skip = null, int? limit = null, bool? withCount = null,
-            string sortField = null, SortDirection? sortDirection = null, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UserWithPasswordHash> GetUserWithPasswordAsync(string id, string membershipId, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UserWithPasswordHash> GetUserWithPasswordAsync(string username, string email, string membershipId, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<DynamicObject> ChangePasswordAsync(Utilizer utilizer, string membershipId, string userId, string newPassword, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResetPasswordToken> ResetPasswordAsync(Utilizer utilizer, string membershipId, string emailAddress, string host, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<User> VerifyResetTokenAsync(string membershipId, string resetToken, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetPasswordAsync(Utilizer utilizer, string membershipId, string resetToken, string usernameOrEmailAddress,
-            string password, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> CheckPasswordAsync(Utilizer utilizer, string password, CancellationToken cancellationToken = default)
+        public Task<bool> CheckPasswordAsync(
+            Utilizer utilizer, 
+            string password, 
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
@@ -156,24 +371,23 @@ namespace ErtisAuth.Tests.Mocks.Services
             throw new NotImplementedException();
         }
 
-        public Task<dynamic> AggregateAsync(string membershipId, string aggregationStagesJson, CancellationToken cancellationToken = default)
+        public Task<dynamic> AggregateAsync(
+            string membershipId, 
+            string aggregationStagesJson, 
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
         
-        public Task<User> ActivateUserAsync(Utilizer utilizer, string membershipId, string activationCode, CancellationToken cancellationToken = default)
+        public Task<string> SendActivationMailAsync(
+            string membershipId, 
+            string userId, 
+            string host = null, 
+            CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
         
-        public Task<User> ActivateUserByIdAsync(Utilizer utilizer, string membershipId, string userId, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-        
-        public Task<User> FreezeUserByIdAsync(Utilizer utilizer, string membershipId, string userId, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
+        #endregion
     }
 }
