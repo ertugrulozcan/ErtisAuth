501ae844c8f1d31108a3fe9076154683ee55a380
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2025-09-01 23:42:25 +0300

    UseHttpsRedirection removed

--- ErtisAuth.WebAPI/Program.cs
+++ ErtisAuth.WebAPI/Program.cs
@@ -226,7 +226,7 @@ if (!string.IsNullOrEmpty(sentryDsn))
 }
 
 // ApplicationInsights
-// builder.Services.AddApplicationInsights(builder.Configuration);
+builder.Services.AddApplicationInsights(builder.Configuration);
 
 // Logging
 builder.Logging.AddJsonConsole(options =>
@@ -266,7 +266,7 @@ app.CheckDatabaseIndexes();
 app.UseMailkit();
 app.UseProviders();
 app.UseCors(CORS_POLICY_KEY);
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
